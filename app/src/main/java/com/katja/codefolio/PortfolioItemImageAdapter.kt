6f166b3fc693c87b98b19bed8133ecfff5e1e879
Katja <katja@klahr.se>
2023-11-17 23:19:47 +0100

    Småfix

--- app/src/main/java/com/katja/codefolio/PortfolioItemImageAdapter.kt
+++ app/src/main/java/com/katja/codefolio/PortfolioItemImageAdapter.kt
@@ -4,21 +4,21 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.katja.codefolio.databinding.PortfolioItemBinding
+import com.katja.codefolio.databinding.PortfolioItemImageBinding
 
 
 class PortfolioItemImageAdapter(
     var portfolioItemImageList: ArrayList<Int>
 ) : RecyclerView.Adapter<PortfolioItemImageAdapter.ViewHolder>() {
 
-    class ViewHolder(binding: PortfolioItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val imImage: ImageView = binding.portfolioItemImage
+    class ViewHolder(binding: PortfolioItemImageBinding) : RecyclerView.ViewHolder(binding.root) {
+        val imImage: ImageView = binding.portfolioItemImages
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
-            PortfolioItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            PortfolioItemImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
